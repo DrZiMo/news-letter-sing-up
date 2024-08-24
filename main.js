cdd750e97456d539950fc676a9bd3e9bcbdb0065
Dr_ZiMo <zuhaybhamar@gmail.com>
2024-08-24 17:55:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+const email = document.getElementById("email");
+const subscribe = document.getElementById("subs");
+const errMsg = document.querySelector(".error-msg");
+const msgContainer = document.querySelector(".message");
+const mainImg = document.getElementById('main-image');
+
+const inValidCharacters = [' ', ',', ';', '"', "'", '!', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '|', '/', '~', '`'];
+const essentialCharacters = ['@', '.'];
+
+msgContainer.style.display = 'none';
+
+function initialize() {
+    msgContainer.style.display = 'none';
+    email.value = '';
+    errMsg.textContent = '';
+
+    if (window.innerWidth < 750) {
+        mainImg.src = 'assets/images/illustration-sign-up-mobile.svg';
+        console.log(window.body.innerWidth);
+    }
+    else {
+        mainImg.src = 'assets/images/illustration-sign-up-desktop.svg';
+    }
+}
+initialize()
+subscribe.addEventListener('click', () => subscribeHandler());
+
+function subscribeHandler() {
+    if (!(email.value === '')) {
+        for (const inValidCharacter in inValidCharacters) {
+            if (email.value.includes(inValidCharacters[inValidCharacter])) {
+                error('Valid email required');
+            }
+            else {
+                for (const essentialCharacter in essentialCharacters) {
+                    if (!(email.value.includes(essentialCharacters[essentialCharacter]))) {
+                        error('Valid email required');
+                    }
+                    else {
+                        subscribeSuccess(email.value);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    else {
+        error('Please enter your email');
+    }
+}
+
+function error(mess) {
+    errMsg.textContent = mess;
+    email.className = 'error';
+}
+
+function subscribeSuccess(email) {
+    const emailMsg = document.getElementById('email-msg');
+    const dismiss = document.getElementById('dismiss');
+    msgContainer.style.display = 'flex';
+    emailMsg.textContent = email;
+
+    dismiss.addEventListener('click', () => initialize());
+}
